02ac9bb41eebf085d290311c662b3283e986faf1
caillette <administrateur@Laptop-0.local>
2008-08-28 11:39:28 +0200

    Code formatting.

--- src/test/novelang/parser/antlr/TreeFixture.java
+++ src/test/novelang/parser/antlr/TreeFixture.java
@@ -83,8 +83,8 @@ public class TreeFixture {
   }
 
   public static void assertEquals(
-      Treepath<SyntacticTree> expected,
-      Treepath<SyntacticTree> actual
+      Treepath< SyntacticTree > expected,
+      Treepath< SyntacticTree > actual
   ) {
     Assert.assertEquals( "Treepath height", expected.getLength(), actual.getLength() ) ;
     for( int i = 0 ; i < expected.getLength() ; i++ ) {
