21a2d13a5b7b691d1e9d79f7ec245089599308ec
caillette13 <Laurent@imac-de-louise.local>
2008-10-28 20:41:19 +0100

    Added stub of a launcher for debuggable grammar as ANTLRWork doesn't work in 1.2.1

---
+++ src/test/novelang/parser/antlr/DebuggableGrammarLauncher.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2008 Laurent Caillette
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package novelang.parser.antlr;
+
+import java.io.IOException;
+
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.TokenSource;
+
+/**
+ */
+public class DebuggableGrammarLauncher {
+
+  public static void main( String[] args ) throws IOException {
+/*
+    TokenSource lex = ( new ANTLRFileStream( "/Users/Laurent/Novelang/antlrworks/__Test___input.txt" ) );
+    CommonTokenStream tokens = new CommonTokenStream( lex );
+
+    NovelangParser g = new NovelangParser( tokens, 49100, null );
+    try {
+      g.textBlock();
+    } catch(
+        RecognitionException e ) {
+      e.printStackTrace();
+    }
+
+*/
+  }
+
+
+}
