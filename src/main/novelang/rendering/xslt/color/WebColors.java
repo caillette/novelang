9acb320df0995635f82a4ae8a759608c0e6dbb03
caillette <administrateur@Laptop-0.local>
2009-04-24 08:37:36 +0200

    Moved WebColors to color subpackage

--- src/main/novelang/rendering/xslt/color/WebColors.java
+++ src/main/novelang/rendering/xslt/color/WebColors.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package novelang.rendering.xslt;
+package novelang.rendering.xslt.color;
 
 import java.awt.Color;
 import java.util.Iterator;
