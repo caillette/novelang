dc39e4841c380ae6845ade364dd3b85aa4e24e6b
caillette <administrateur@Laptop-0.local>
2010-05-17 22:41:45 +0200

    Work in progress: creating assembly with Maven.

--- modules/bootstrap/src/main/java/novelang/bootstrap/Main.java
+++ modules/bootstrap/src/main/java/novelang/bootstrap/Main.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package novelang.launcher;
+package novelang.bootstrap;
 
 import java.util.Map;
 import java.io.PrintStream;
